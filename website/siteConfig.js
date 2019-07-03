4d1d69f38ffd4a78e5f8e1aad5539c192e180542
orkunkl <kulceorkun@gmail.com>
2019-07-03 13:26:09 +0300

    Remove unnecessary docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -20,7 +20,7 @@ const siteConfig = {
 
   headerLinks: [
     {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+  //  {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],

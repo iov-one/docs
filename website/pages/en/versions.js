9086065941a89cb21859d3a889eb96f2290cbcd5
Orkun Külçe <kulceorkun@gmail.com>
2019-10-29 15:26:03 +0300

    remove outdated

--- website/pages/en/versions.js
+++ website/pages/en/versions.js
@@ -40,7 +40,7 @@ function Versions(props) {
                   <a
                     href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
                       props.language ? props.language + '/' : ''
-                    }doc1`}>
+                    }/intro`}>
                     Documentation
                   </a>
                 </td>
@@ -65,7 +65,7 @@ function Versions(props) {
                   <a
                     href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
                       props.language ? props.language + '/' : ''
-                    }next/doc1`}>
+                    }next/intro`}>
                     Documentation
                   </a>
                 </td>
@@ -91,7 +91,7 @@ function Versions(props) {
                         <a
                           href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
                             props.language ? props.language + '/' : ''
-                          }${version}/doc1`}>
+                          }${version}/intro`}>
                           Documentation
                         </a>
                       </td>
